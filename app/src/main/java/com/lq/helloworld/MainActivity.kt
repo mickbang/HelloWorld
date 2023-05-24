1987ca8329724fea122d58950535a2077b10f063
mick <l506945024@163.com>
2023-05-24 17:36:14 +0800

    scanner

--- app/src/main/java/com/lq/helloworld/MainActivity.kt
+++ app/src/main/java/com/lq/helloworld/MainActivity.kt
@@ -1,11 +1,100 @@
 package com.lq.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import com.dynamsoft.dbr.BarcodeReader
+import com.dynamsoft.dbr.ImageData
+import com.dynamsoft.dbr.TextResult
+import com.dynamsoft.dbr.TextResultListener
+import com.dynamsoft.dce.CameraEnhancer
+import com.dynamsoft.dce.CameraEnhancerException
+import com.lq.helloworld.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity(), TextResultListener {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(LayoutInflater.from(this@MainActivity))
+    }
+    private val mCameraEnhancer by lazy {
+        CameraEnhancer(this@MainActivity)
+    }
+    private val mBarcodeReader by lazy {
+        BarcodeReader()
+    }
+
+    private val handler = Handler(Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        mCameraEnhancer.cameraView = binding.cameraView
+//        binding.cameraView.overlayVisible = true
+//        binding.cameraView.removeOverlay()
+        mBarcodeReader.setCameraEnhancer(mCameraEnhancer)
+
+        BarcodeReader.initLicense(
+            "f0068NQAAAGbuhFaxQ5FxER/RBRir4icpyVRfBvZw2k2VmkqTz4wpxiGZZAN4v3lp2/qPhv6zhQGWhr1tI1sKkVu/PF9WaLs=;f0068NQAAAJJBpnkl5wyvM2lftdrSMEs2j3mnPqFmInsxkW7R/m9kRTpcRAP8VY5dAqk9eQ0vvZLKN3fBeOEeJQnKWNJbLGY="
+        ) { p0, p1 ->
+            if (p0) {
+                Log.d(TAG, "onCreate: init success")
+            } else {
+                Log.d(TAG, "onCreate: init fail ====>fail reason:${p1.message}")
+            }
+
+        }
+        mBarcodeReader.setTextResultListener(this)
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        try {
+            mCameraEnhancer.open()
+            mBarcodeReader.startScanning()
+        } catch (e: CameraEnhancerException) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        try {
+            mCameraEnhancer.close()
+            mBarcodeReader.stopScanning()
+        } catch (e: CameraEnhancerException) {
+            e.printStackTrace()
+        }
+    }
+
+    var sameCount = 1
+    var lastResult = ""
+    override fun textResultCallback(p0: Int, p1: ImageData?, p2: Array<out TextResult>?) {
+        if (!p2.isNullOrEmpty()) {
+            val ss = java.lang.StringBuilder()
+            p2.forEach {
+                ss.append(it.barcodeText + "\n")
+            }
+            Log.d(TAG, "textResultCallback: $ss")
+           runOnUiThread {
+               try {
+                   if (lastResult == ss.toString()) {
+                       sameCount++
+                       binding.tvResult.text = "$sameCount \n$ss"
+                   } else {
+                       sameCount = 1
+                       lastResult = ss.toString()
+                       binding.tvResult.text = ss
+                   }
+               }catch (e:Exception){
+                   e.printStackTrace()
+               }
+           }
+        }
     }
 }
