1b9d23246bdd1351712c18280d472d8be15da351
mick <l506945024@163.com>
2023-12-08 14:38:50 +0800

    data

--- app/src/main/java/com/lq/helloworld/MainActivity.kt
+++ app/src/main/java/com/lq/helloworld/MainActivity.kt
@@ -17,27 +17,27 @@ class MainActivity : AppCompatActivity() {
         binding.apply {
             btnWrite.setOnClickListener {
                 val dir = getExternalFilesDir(null)?.absolutePath + "/Photo/"
-                var filepath = dir + "WechatIMG17297.jpg"
-                println("执行路径：${filepath}")
+//                var filepath = dir + "WechatIMG17297.jpg"
+//                println("执行路径：${filepath}")
+//                ImageExifUtils.writeImageExif(
+//                    filepath,
+//                    ImageExif("2023-09-26 12:27:08", -27.5825793,152.9825345
+//                    )
+//                )
+//                println("写入geo结果：${ImageExifUtils.getImageExif(filepath)}")
                 ImageExifUtils.writeImageExif(
-                    filepath,
-                    ImageExif("2023-09-26 12:27:08", -27.5825793,152.9825345
-                    )
+                    dir + "1.png",
+                    ImageExif("2023-03-08 09:25:09", -28.2391242,153.4977817)
                 )
-                println("写入geo结果：${ImageExifUtils.getImageExif(filepath)}")
-//                ImageExifUtils.writeImageExif(
-//                    dir + "IMG_HEAD_257875_installer.png",
-//                    ImageExif("2023-03-08 09:25:09", -28.2391242,153.4977817)
-//                )
-//                ImageExifUtils.writeImageExif(
-//                    dir + "1669703074578.png",
-//                    ImageExif("", -12.390869, 130.8668757)
-//                )
-//
-//                ImageExifUtils.writeImageExif(
-//                    dir + "1673399604476.png",
-//                    ImageExif("", -35.0646301 ,138.8834305)
-//                )
+                ImageExifUtils.writeImageExif(
+                    dir + "3.png",
+                    ImageExif("2023-03-08 09:25:09", -12.390869, 130.8668757)
+                )
+
+                ImageExifUtils.writeImageExif(
+                    dir + "4.png",
+                    ImageExif("2023-03-08 09:25:09", -35.0646301 ,138.8834305)
+                )
 
 //                ImageExifUtils.writeImageExif(
 //                    dir + "Dec_05_2022_13_34_41179.png",
